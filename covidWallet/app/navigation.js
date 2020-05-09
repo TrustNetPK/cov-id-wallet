e7816b252b554ea014f206495d46431c69ebbbca
saeed_dev <saeed_dev@yahoo.com>
2020-05-09 07:22:45 +0500

    added icons to topBar

--- covidWallet/app/navigation.js
+++ covidWallet/app/navigation.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './components/TabNavigator';
 import SplashScreen from 'react-native-splash-screen';
 import PassCodeScreen from './containers/PassCodeScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 
@@ -16,10 +17,24 @@ function NavigationComponent() {
     <NavigationContainer>
        <Stack.Navigator initialRouteName="PassCodeScreen">
         <Stack.Screen options={{headerShown: false}} name="PassCodeScreen" component={PassCodeScreen}  />
-        <Stack.Screen name="MainScreen"  options={{ title: 'COVID WALLET' }} component={TabNavigator}  />
+        <Stack.Screen name="MainScreen"  options={{  headerStyle: {
+      elevation: 0,
+      shadowOpacity: 0,
+      borderBottomWidth: 0,
+    }, title: false,headerRight: () => (
+        <Icon onPress={()=>{console.log('1')}} style={styles.headerRightIcon} size={30} name="qrcode" padding={30} />
+      ),headerLeft: () => (
+        <Icon onPress={()=>{console.log('backbutton')}} style={styles.headerRightIcon} size={30} name="gear" padding={30} />
+      ) }} component={TabNavigator}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  headerRightIcon: {
+    padding: 10,
+  }
+});
 
 export default NavigationComponent;
