e5f4f9521c7b24732bbc8abba758e48b12892f12
saeed_dev <saeed_dev@yahoo.com>
2020-05-10 17:11:01 +0500

    added FlatCard to actions and connections screen

--- covidWallet/app/components/ActionsScreen.js
+++ covidWallet/app/components/ActionsScreen.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import FlatCard from './FlatCard';
+
+const image = require('../assets/images/visa.jpg')
 
 function ActionsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>ActionsScreen</Text>
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Text>Actions</Text>
+      <FlatCard image={image} heading="VISA MasterCard" text="Mastercard enables you to pay at 2 billion plus kiosks throughout the world"/>
+      <FlatCard image={image} heading="VISA MasterCard" text="Mastercard enables you to pay at 2 billion plus kiosks throughout the world"/>
     </View>
   );
 }
--- covidWallet/app/components/ConnectionsScreen.js
+++ covidWallet/app/components/ConnectionsScreen.js
@@ -1,11 +1,17 @@
 
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import FlatCard from './FlatCard';
+
+const image = require('../assets/images/visa.jpg')
 
 function ConnectionsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center' }}>
       <Text>ConnectionsScreen</Text>
+      <FlatCard image={image} heading="VISA MasterCard" text="Mastercard enables you to pay at 2 billion plus kiosks throughout the world"/>
+      <FlatCard image={image} heading="VISA MasterCard" text="Mastercard enables you to pay at 2 billion plus kiosks throughout the world"/>
+
     </View>
   );
 }
