a63061a231666eb17fd18971e15e3e8238d38ae6
Safi <safiawan099@gmail.com>
2020-06-19 13:19:36 +0500

    Status Bar color Modification

--- covidWallet/App.js
+++ covidWallet/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { StatusBar } from "react-native";
 import NavigationComponent from './app/Navigation';
+import { BLACK_COLOR, PRIMARY_COLOR, SECONDARY_COLOR } from './app/theme/Colors';
 
 const App = () => {
   return (
     <>
+      <StatusBar barStyle="light-content" backgroundColor={BLACK_COLOR} />
       <NavigationComponent />
     </>
   );
