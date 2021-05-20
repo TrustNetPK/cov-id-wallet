5a2cecab31e21c09cbf723cc71f3786c46342310
iamsafidev <safiawan099@gmail.com>
2021-05-21 03:19:26 +0500

    Register Module Completed

--- covidWallet/app/components/HeadingComponent.js
+++ covidWallet/app/components/HeadingComponent.js
@@ -1,25 +1,22 @@
 import * as React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { BLACK_COLOR } from '../theme/Colors';
+import {Text, StyleSheet} from 'react-native';
+import {BLACK_COLOR} from '../theme/Colors';
 
 function HeadingComponent(props) {
-
-    const styles = StyleSheet.create({
-        heading: {
-            fontSize: 30,
-            fontFamily: 'Merriweather-Bold',
-            marginTop: 20,
-            marginBottom: 15,
-            color: BLACK_COLOR,
-            justifyContent: 'center',
+  const styles = StyleSheet.create({
+    heading: {
+      fontSize: 30,
+      fontFamily: 'Merriweather-Bold',
+      marginTop: 20,
+      marginBottom: 15,
+      color: BLACK_COLOR,
+      justifyContent: 'center',
       alignItems: 'center',
       textAlign: 'center',
-        }
-    });
+    },
+  });
 
-    return (
-        <Text style={styles.heading}>{props.text}</Text>
-    )
+  return <Text style={styles.heading}>{props.text}</Text>;
 }
 
 export default HeadingComponent;
