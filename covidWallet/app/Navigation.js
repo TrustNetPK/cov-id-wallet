fad85d523e149fa0293a567fc42ab3c08395d2a6
iamsafidev <safiawan099@gmail.com>
2021-06-08 03:36:06 +0500

    Remove the jerks between the navigation Stack by adding Loading Screen between them

--- covidWallet/app/Navigation.js
+++ covidWallet/app/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -19,11 +19,13 @@ import QRScreen from './screens/QRScreen';
 import {BLACK_COLOR, BACKGROUND_COLOR} from './theme/Colors';
 import RegistrationModule from './screens/RegistrationModule';
 import MultiFactorScreen from './screens/MultiFactorScreen';
+import LoadingScreen from './screens/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 function NavigationComponent() {
   const [isFirstTime, getisFirstTime] = React.useState('true');
+  const [isLoading, setLoading] = useState(true);
   const storeData = async () => {
     try {
       await AsyncStorage.setItem('isfirstTime', 'false');
@@ -31,31 +33,17 @@ function NavigationComponent() {
       // Error saving data
     }
   };
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case 'FIRST_TIME':
-          return {
-            ...prevState,
-            isFirstTime: 'true',
-          };
-      }
-    },
-    {
-      isFirstTime: true,
-    },
-  );
 
   const retrieveData = async () => {
     try {
       const value = await AsyncStorage.getItem('isfirstTime').then(value => {
+        setLoading(false);
         console.log('Local Storage Values is ' + value);
-        // if (value == null) {
-        //   getisFirstTime("true")
-        // }
-        // else {
-        getisFirstTime(value);
-        // }
+        if (value == null) {
+          getisFirstTime('true');
+        } else {
+          getisFirstTime(value);
+        }
 
         console.log('Value isa ' + isFirstTime);
       });
@@ -84,101 +72,107 @@ function NavigationComponent() {
   return (
     <AuthContext.Provider value={authContext}>
       <NavigationContainer>
-        <Stack.Navigator>
-          {isFirstTime == null || isFirstTime == true ? (
-            <>
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="WelcomeScreen"
-                component={WelcomeScreen}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="RegistrationScreen"
-                component={RegistrationModule}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="MultiFactorScreen"
-                component={MultiFactorScreen}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="PassCodeContainer"
-                component={PassCodeContainer}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="SecurityScreen"
-                component={SecurityScreen}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="SecureidContainer"
-                component={SecureIdContainer}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="NotifyMeScreen"
-                component={NotifyMeScreen}
-              />
-            </>
-          ) : (
-            <>
-              <Stack.Screen
-                name="MainScreen"
-                options={({navigation}) => ({
-                  headerStyle: {
-                    backgroundColor: BACKGROUND_COLOR,
-                    elevation: 0,
-                    shadowOpacity: 0,
-                    borderBottomWidth: 0,
-                  },
-                  title: false,
-                  headerLeft: () => (
-                    <FontAwesome
-                      onPress={() => {
-                        navigation.navigate('SettingsScreen');
-                      }}
-                      style={styles.headerRightIcon}
-                      size={30}
-                      name="navicon"
-                      padding={30}
-                    />
-                  ),
-                })}
-                component={TabNavigator}
-              />
-              <Stack.Screen
-                name="SettingsScreen"
-                options={({navigation}) => ({
-                  headerLeft: () => (
-                    <MaterialIcons
-                      onPress={() => {
-                        navigation.goBack();
-                      }}
-                      style={styles.headerRightIcon}
-                      size={30}
-                      name="arrow-back"
-                      padding={30}
-                    />
-                  ),
-                })}
-                component={SettingsScreen}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="QRScreen"
-                component={QRScreen}
-              />
-              <Stack.Screen
-                options={{headerShown: false}}
-                name="AuthenticationContainer"
-                component={AuthenticationContainer}
-              />
-            </>
-          )}
-        </Stack.Navigator>
+        {isLoading ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="LoadingScreen"
+              component={LoadingScreen}
+            />
+          </Stack.Navigator>
+        ) : isFirstTime === 'true' ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="WelcomeScreen"
+              component={WelcomeScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="RegistrationScreen"
+              component={RegistrationModule}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="MultiFactorScreen"
+              component={MultiFactorScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="PassCodeContainer"
+              component={PassCodeContainer}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="SecurityScreen"
+              component={SecurityScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="SecureidContainer"
+              component={SecureIdContainer}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="NotifyMeScreen"
+              component={NotifyMeScreen}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="MainScreen"
+              options={({navigation}) => ({
+                headerStyle: {
+                  backgroundColor: BACKGROUND_COLOR,
+                  elevation: 0,
+                  shadowOpacity: 0,
+                  borderBottomWidth: 0,
+                },
+                title: false,
+                headerLeft: () => (
+                  <FontAwesome
+                    onPress={() => {
+                      navigation.navigate('SettingsScreen');
+                    }}
+                    style={styles.headerRightIcon}
+                    size={30}
+                    name="navicon"
+                    padding={30}
+                  />
+                ),
+              })}
+              component={TabNavigator}
+            />
+            <Stack.Screen
+              name="SettingsScreen"
+              options={({navigation}) => ({
+                headerLeft: () => (
+                  <MaterialIcons
+                    onPress={() => {
+                      navigation.goBack();
+                    }}
+                    style={styles.headerRightIcon}
+                    size={30}
+                    name="arrow-back"
+                    padding={30}
+                  />
+                ),
+              })}
+              component={SettingsScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="QRScreen"
+              component={QRScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="AuthenticationContainer"
+              component={AuthenticationContainer}
+            />
+          </Stack.Navigator>
+        )}
       </NavigationContainer>
     </AuthContext.Provider>
   );
--- covidWallet/app/screens/ActionsScreen.js
+++ covidWallet/app/screens/ActionsScreen.js
@@ -1,83 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { useFocusEffect, CommonActions } from '@react-navigation/native';
-import { View, Text, Image, StyleSheet, TouchableOpacityBase, Alert,ImageBackground } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {useFocusEffect, CommonActions} from '@react-navigation/native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacityBase,
+  Alert,
+  ImageBackground,
+} from 'react-native';
 import FlatCard from '../components/FlatCard';
 import ImageBoxComponent from '../components/ImageBoxComponent';
 import TextComponent from '../components/TextComponent';
 import HeadingComponent from '../components/HeadingComponent';
-import { themeStyles } from '../theme/Styles';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {themeStyles} from '../theme/Styles';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import ModalComponent from '../components/ModalComponent';
-import { getItem, deleteActionByConnId, saveItem } from '../helpers/Storage';
-import { authenticate } from '../helpers/Authenticate';
+import {getItem, deleteActionByConnId, saveItem} from '../helpers/Storage';
+import {authenticate} from '../helpers/Authenticate';
 import ConstantsList from '../helpers/ConfigApp';
-import { ScrollView } from 'react-native-gesture-handler';
+import {ScrollView} from 'react-native-gesture-handler';
 import BorderButton from '../components/BorderButton';
-const image = require('../assets/images/visa.jpg')
+const image = require('../assets/images/visa.jpg');
 
 function ActionsScreen({navigation}) {
-  const [isAction, setAction] = useState(true);
-  const [isModalVisible, setModalVisible] = useState(true);
+  const [isAction, setAction] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
   const [actionsList, setActionsList] = useState([]);
   const [modalData, setModalData] = useState([]);
   const [selectedItem, setSelectedItem] = useState('');
 
   useFocusEffect(
     React.useCallback(() => {
-      updateActionsList()
-      return
-    }, [isAction])
+      updateActionsList();
+      return;
+    }, [isAction]),
   );
 
   const updateActionsList = () => {
-    getItem(ConstantsList.CERT_REQ).then((actions) => {
-      if (actions != null) {
-        let credActionsList = JSON.parse(actions)
-        return credActionsList
-      }
-    }).then(credlist => {
-      var finalObj = []
-      if (credlist != undefined) {
-        finalObj = finalObj.concat(credlist);
-      }
-      getItem(ConstantsList.PROOF_REQ).then((actions2) => {
-        if (actions2 != null) {
-          let proofActionsList = JSON.parse(actions2)
-          finalObj = finalObj.concat(proofActionsList);
-        }
-
-        if (finalObj.length > 0) {
-          setActionsList(finalObj);
-          setAction(true);
-        } else {
-          setAction(false);
+    getItem(ConstantsList.CERT_REQ)
+      .then(actions => {
+        if (actions != null) {
+          let credActionsList = JSON.parse(actions);
+          return credActionsList;
         }
       })
+      .then(credlist => {
+        var finalObj = [];
+        if (credlist != undefined) {
+          finalObj = finalObj.concat(credlist);
+        }
+        getItem(ConstantsList.PROOF_REQ).then(actions2 => {
+          if (actions2 != null) {
+            let proofActionsList = JSON.parse(actions2);
+            finalObj = finalObj.concat(proofActionsList);
+          }
 
-    })
-      .catch(e => { })
-  }
+          if (finalObj.length > 0) {
+            setActionsList(finalObj);
+            setAction(true);
+          } else {
+            setAction(false);
+          }
+        });
+      })
+      .catch(e => {});
+  };
 
-  const toggleModal = (v) => {
-    setSelectedItem(JSON.stringify(v))
+  const toggleModal = v => {
+    setSelectedItem(JSON.stringify(v));
     setModalData(v.data);
     setModalVisible(true);
   };
 
-  const acceptModal = async (v) => {
+  const acceptModal = async v => {
     let selectedItemObj = JSON.parse(selectedItem);
     //Move item to connection screen
     let conns = [];
     let data = await getItem(ConstantsList.CONNECTIONS);
     if (data == null) {
       conns = conns.concat(selectedItemObj);
-    }
-    else {
+    } else {
       try {
         conns = JSON.parse(data);
         conns = conns.concat(selectedItemObj);
-      }
-      catch (e) {
+      } catch (e) {
         conns = [];
       }
     }
@@ -90,94 +97,114 @@ function ActionsScreen({navigation}) {
       let walletSecret = await getItem(ConstantsList.WALLET_SECRET);
 
       //make API call
-      await fetch(ConstantsList.BASE_URL + `/connections/receive-invitation`,
-        {
-          method: 'POST',
-          headers: {
-            'X-API-Key': ConstantsList.API_SECRET,
-            'Content-Type': 'application/json; charset=utf-8',
-            'Server': 'Python/3.6 aiohttp/3.6.2',
-            'wallet-name': walletName,
-            'wallet-key': walletSecret
-          },
-          body: JSON.stringify(selectedItemObj.invitation.invitation)
-        }).then(inviteResult =>
-          inviteResult.json().then(data => {
-            // console.log(data)
-          }));
+      await fetch(ConstantsList.BASE_URL + `/connections/receive-invitation`, {
+        method: 'POST',
+        headers: {
+          'X-API-Key': ConstantsList.API_SECRET,
+          'Content-Type': 'application/json; charset=utf-8',
+          Server: 'Python/3.6 aiohttp/3.6.2',
+          'wallet-name': walletName,
+          'wallet-key': walletSecret,
+        },
+        body: JSON.stringify(selectedItemObj.invitation.invitation),
+      }).then(inviteResult =>
+        inviteResult.json().then(data => {
+          // console.log(data)
+        }),
+      );
       //Remove item from actions
       setModalVisible(false);
-      deleteActionByConnId(selectedItemObj.type, selectedItemObj.invitation.connection_id).then((actions) => {
+      deleteActionByConnId(
+        selectedItemObj.type,
+        selectedItemObj.invitation.connection_id,
+      ).then(actions => {
         updateActionsList();
       });
-    }
-    else {
+    } else {
       // do nothing
       setModalVisible(false);
     }
+  };
 
-  }
-
-  const rejectModal = (v) => {
+  const rejectModal = v => {
     let selectedItemObj = JSON.parse(selectedItem);
     setModalVisible(false);
-    deleteActionByConnId(selectedItemObj.type, selectedItemObj.invitation.connection_id).then((actions) => {
+    deleteActionByConnId(
+      selectedItemObj.type,
+      selectedItemObj.invitation.connection_id,
+    ).then(actions => {
       updateActionsList();
     });
+  };
 
-  }
-
-  const dismissModal = (v) => {
+  const dismissModal = v => {
     setModalVisible(false);
   };
 
   return (
     <View style={themeStyles.mainContainer}>
       <HeadingComponent text="Actions" />
-      {isAction &&
+      {isAction && (
         <View>
           <ScrollView showsVerticalScrollIndicator={true}>
-            <ModalComponent data={modalData} isVisible={isModalVisible} toggleModal={toggleModal} rejectModal={rejectModal} dismissModal={dismissModal} acceptModal={acceptModal} modalType="action" />
-            {
-              actionsList !== undefined && actionsList.map((v, i) => {
-                let header = String(v.type === "connection_credential" ? "Digital Certificate Request" : "Digital Proof Request");
-                let subtitle = "Click to view the " + header.toLowerCase() + " from " + v.org.name;
-                let imgURI = { uri: v.org.img };
-                return <TouchableOpacity key={i} onPress={() => toggleModal(v)}>
-                  <FlatCard image={imgURI} heading={header} text={subtitle} />
-                </TouchableOpacity>
-              })
-            }
+            <ModalComponent
+              data={modalData}
+              isVisible={isModalVisible}
+              toggleModal={toggleModal}
+              rejectModal={rejectModal}
+              dismissModal={dismissModal}
+              acceptModal={acceptModal}
+              modalType="action"
+            />
+            {actionsList !== undefined &&
+              actionsList.map((v, i) => {
+                let header = String(
+                  v.type === 'connection_credential'
+                    ? 'Digital Certificate Request'
+                    : 'Digital Proof Request',
+                );
+                let subtitle =
+                  'Click to view the ' +
+                  header.toLowerCase() +
+                  ' from ' +
+                  v.org.name;
+                let imgURI = {uri: v.org.img};
+                return (
+                  <TouchableOpacity key={i} onPress={() => toggleModal(v)}>
+                    <FlatCard image={imgURI} heading={header} text={subtitle} />
+                  </TouchableOpacity>
+                );
+              })}
           </ScrollView>
         </View>
-      }
-      {!isAction &&
+      )}
+      {!isAction && (
         <View style={styles.EmptyContainer}>
           <TextComponent text="There are no actions to complete, Please scan a QR code to either get a digital certificate or to prove it." />
           <ImageBoxComponent source={require('../assets/images/action.png')} />
-          <TouchableOpacity activeOpacity={0.9} onPress={() => {
-                          navigation.navigate('QRScreen');
-                        }}>
-                          <BorderButton text="QR CODE"/>
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={() => {
+              navigation.navigate('QRScreen');
+            }}>
+            <BorderButton text="QR CODE" />
           </TouchableOpacity>
-          
         </View>
-      }
+      )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   EmptyContainer: {
-    flex:1,
-    alignItems: 'center'
+    flex: 1,
+    alignItems: 'center',
   },
   bottom: {
-    width: 50, height: 50
+    width: 50,
+    height: 50,
   },
-  imageProps:{
-     
-  }
+  imageProps: {},
 });
 
 export default ActionsScreen;
