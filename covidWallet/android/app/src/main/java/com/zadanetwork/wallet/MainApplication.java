ba6664af43dfb90bc160fdc9c70b28de831b89a2
Haseeb Ahmed <haseeba393@gmail.com>
2021-08-16 13:27:58 +0500

    remove credentials

--- covidWallet/android/app/src/main/java/com/zadanetwork/wallet/MainApplication.java
+++ covidWallet/android/app/src/main/java/com/zadanetwork/wallet/MainApplication.java
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     Fresco.initialize(this);
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    //initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
