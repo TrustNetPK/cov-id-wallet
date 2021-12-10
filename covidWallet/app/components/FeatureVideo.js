ab840e10a3b8e6be708f66da8b0ddd20002168ac
Haseeb Ahmed <haseeba393@gmail.com>
2021-12-10 21:19:09 +0500

    video library changed

--- covidWallet/app/components/FeatureVideo.js
+++ covidWallet/app/components/FeatureVideo.js
@@ -1,22 +1,12 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React from 'react'
 import { ActivityIndicator, Dimensions, StyleSheet, View } from 'react-native'
 import Modal from 'react-native-modal';
-import YoutubePlayer from "react-native-youtube-iframe";
 import { GRAY_COLOR, WHITE_COLOR } from '../theme/Colors';
 import SimpleButton from './Buttons/SimpleButton';
 import HeadingComponent from './HeadingComponent';
+import WebView from 'react-native-webview';
 
 const FeatureVideo = ({ isVisible, onCloseClick }) => {
-
-    const playerRef = useRef();
-
-    const onStateChange = useCallback((state) => {
-        if (state == "ended") {
-            playerRef.current.seekTo(0, true);
-        }
-    }, []);
-
-    const [loading, setLoading] = useState(true);
 
     return (
         <Modal
@@ -26,42 +16,32 @@ const FeatureVideo = ({ isVisible, onCloseClick }) => {
                 <HeadingComponent
                     text={`Introducing\nnew feature`}
                 />
-
-                {
-                    loading &&
-                    <ActivityIndicator
-                        size='large'
-                        color={GRAY_COLOR}
-                        style={{
-                            position: 'absolute',
-                            top: '50%',
-                            alignSelf: 'center',
+                <View style={{
+                    width: '100%',
+                    height: Dimensions.get('window').width * 0.5,
+                    borderRadius: 15,
+                }}>
+                    <WebView
+                        renderLoading={() => (
+                            <ActivityIndicator
+                                size='large'
+                                color={GRAY_COLOR}
+                                style={{
+                                    alignSelf: 'center',
+                                }}
+                            />
+                        )}
+                        bounces={false}
+                        originWhitelist={['*']}
+                        allowsInlineMediaPlayback
+                        mediaPlaybackRequiresUserAction={false}
+                        source={{ uri: 'https://www.youtube.com/embed/bOcJZZapxKw?playlist=bOcJZZapxKw&loop=1&autoplay=1&controls=0&fs=0&playsinline=1' }}
+                        allowsFullscreenVideo={false}
+                        containerStyle={{
+                            borderRadius: 15,
                         }}
                     />
-                }
-                <YoutubePlayer
-                    ref={playerRef}
-                    width={'100%'}
-                    height={Dimensions.get('window').width * 0.45}
-                    onReady={() => {
-                        setLoading(false);
-                    }}
-                    play={true}
-                    initialPlayerParams={{
-                        controls: false,
-                        loop: 1,
-
-                    }}
-                    webViewProps={{
-                        style: {
-                            borderRadius: 12,
-                        },
-                    }}
-                    forceAndroidAutoplay={true}
-                    contentScale={0.6}
-                    videoId={"bOcJZZapxKw"}
-                    onChangeState={onStateChange}
-                />
+                </View>
 
                 <SimpleButton
                     onPress={onCloseClick}
