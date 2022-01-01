7b18ff13a26ffe8215c9499a393fe2db1dfb7943
Haseeb Ahmed <haseeba393@gmail.com>
2022-01-02 01:36:28 +0500

    ZM-214 patch integrated successfully

--- covidWallet/app/helpers/time.js
+++ covidWallet/app/helpers/time.js
@@ -1,22 +1,31 @@
 import moment from 'moment';
 
 export const get_local_issue_time = (issueTime) => {
+
     let isValid = moment.utc(issueTime, 'mm/dd/yyyy hh:mm:ss').isValid();
     if (isValid) {
         return moment.utc(issueTime, 'MM/DD/YYYY hh:mm:ss').local().format("DD/MM/YYYY hh:mm A").toString();
     }
     else {
+        let formattedDate = moment(issueTime).format('DD/MM/YYYY HH:MM A');
+        if (formattedDate !== 'Invalid date') {
+            return formattedDate;
+        }
         return issueTime;
     }
 }
 
 export const get_local_issue_date = (issueTime) => {
+
     let isValid = moment.utc(issueTime, 'mm/dd/yyyy hh:mm:ss').isValid();
-    console.log(isValid);
     if (isValid) {
         return moment.utc(issueTime, 'MM/DD/YYYY hh:mm:ss').local().format("DD/MM/YYYY").toString();
     }
     else {
+        let formattedDate = moment(issueTime).format('DD/MM/YYYY');
+        if (formattedDate !== 'Invalid date') {
+            return formattedDate;
+        }
         return issueTime;
     }
 }
