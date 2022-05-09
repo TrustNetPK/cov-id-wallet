c7656ccd00c4fe5ecab82e6d904e7039e7b18aa6
samin-labz <saddad@auroralabz.com>
2022-05-09 10:55:03 +0500

    bug(Biometric): ZM-271 Infinite biometric loop bug

--- covidWallet/app/Navigation.js
+++ covidWallet/app/Navigation.js
@@ -85,6 +85,7 @@ function NavigationComponent() {
     } catch (error) {
       // Error retrieving data
     }
+    await AsyncStorage.setItem("temporarilyMovedToBackground", 'false');
   };
 
   // Checking auth status
